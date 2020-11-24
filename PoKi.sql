eafc500d4414348a6ded6a2f9e1ebeeee75daa24
Faith Magras <bdanielle092@gmail.com>
2020-11-24 12:00:56 -0600

    Working on #15

--- PoKi.sql
+++ PoKi.sql
@@ -1,2 +1,9 @@
 ﻿--1.What grades are stored in the database?
-SELECT Name FROM Grade;
+--SELECT Name FROM Grade;
+--2.What emotions may be associated with a poem?
+SELECT Emotion.Name 
+FROM Poem
+LEFT JOIN PoemEmotion on poem.PoemId = Poem.Id
+LEFT JOIN Emotion on PoemEmotion.EmotionId = Emotion.Id;
+--3.How many poems are in the database?
+--SELECT COUNT (Id) AS NumberOfPoem from Poem;
